d86f52a03323fcdf68099767ee6121381e204828
huseyin28 <huseyin2219@gmail.com>
2018-12-13 22:42:33 +0300

    dosya denemesi

---
+++ giris.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+   <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <title>Giriş Yap | X A.Ş. | Stok Yönetim Uygulaması</title>
+   <meta name="description" content="">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta name="robots" content="all,follow">
+   <link rel="stylesheet" href="dist/vendor/bootstrap/css/bootstrap.min.css">
+   <link rel="stylesheet" href="dist/vendor/font-awesome/css/font-awesome.min.css">
+   <link rel="stylesheet" href="dist/css/font.css">
+   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli:300,400,700">
+   <link rel="stylesheet" href="dist/css/style.default.css" id="theme-stylesheet">
+   <link rel="stylesheet" href="dist/css/custom.css">
+   <link rel="shortcut icon" href="dist/img/icon.png">
+   <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+</head>
+
+<body>
+   <div class="login-page">
+      <div class="container d-flex align-items-center">
+         <div class="form-holder has-shadow">
+            <div class="row">
+               <div class="col-lg-6">
+                  <div class="info d-flex align-items-center">
+                     <div class="content">
+                        <div class="logo">
+                           <h1>Stok Yönetimi</h1>
+                        </div>
+                        <p>Şirketinizi, çalışan, müşteri ve stok kayıtlarınızı tutun.</p>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-lg-6 bg-white">
+                  <div class="form d-flex align-items-center">
+                     <div class="content">
+                        <div class="form-validate">
+                           <div class="form-group">
+                              <?php 
+                              if(!empty($_COOKIE['mail'])){
+                                 echo '
+                              <input id="login-mail" type="mail" class="input-material" value="'.$_COOKIE['mail'].'">
+                              <label for="login-mail" class="label-material active">E-posta</label>';
+                              }else{
+                                 echo '
+                                 <input id="login-mail" type="text" class="input-material">
+                                 <label for="login-mail" class="label-material">E-posta</label>';
+                              }?>
+                              
+                           </div>
+                           <div class="form-group">
+                               <?php 
+                              if(!empty($_COOKIE['password']))
+                                 echo '
+                              <input id="login-password" type="password" class="input-material" value="'.$_COOKIE['password'].'">
+                              <label for="login-password" class="label-material active">Şifre</label>';
+                              else
+                                 echo '
+                              <input id="login-password" type="password" class="input-material">
+                              <label for="login-password" class="label-material">Şifre</label>';
+                              ?>
+                              
+                           </div>
+                           <button id="btnLogin" onclick="fnLogin()" class="btn btn-primary">Giriş Yap</button>
+                           <div class="i-checks" style="padding-left: 3px; padding-top: 10px;">
+                            <input id="rememberme" type="checkbox" value="" <?php if(!empty($_COOKIE['password'])) echo 'checked'; ?> class="checkbox-template">
+                            <label for="rememberme">Beni Hatırla</label>
+                          </div>
+                        </div>
+                        <a href="#" class="forgot-pass">Şifreni unuttun mu?</a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="copyrights text-center">
+         <p>Design by <a href="https://instagram.com/huseyin.yilmaz.28" target="_blank" class="external">Hüseyin Yılmaz</a></p>
+      </div>
+   </div>
+   <script src="dist/vendor/jquery/jquery.min.js"></script>
+   <script src="dist/vendor/bootstrap/js/bootstrap.min.js"></script>
+   <script src="dist/vendor/jquery-validation/jquery.validate.min.js"></script>
+   <script src="dist/vendor/jquery.cookie/jquery.cookie.js"> </script>
+   <script src="dist/js/front.js"></script>
+   <script src="dist/js/hy.js"></script>
+   <script>
+      $(document).ready(ready);
+      var d = 1;
+
+      function ready() {
+         $('#btnLogin').on('click', fnLogin);
+         $('#login-password, #login-mail').keyup(function(e){
+             if(e.keyCode == 13){
+                fnLogin();
+             }
+         });
+      }
+
+      function fnLogin() {
+         var data = {
+            mail: $('#login-mail').val().trim(),
+            pass: $('#login-password').val().trim(),
+            rememberme: $('#rememberme').prop('checked')
+         };
+         $.ajax({
+            url: 'services/service.php',
+            dataType : "json",
+            data: {
+               s: 'general',
+               f: 'login',
+               p: data
+            },
+            success: function(res) {
+               if (res.success) {
+                  window.location.assign("./");
+               } else {
+                  alert(res.message);
+               }
+            }
+         });
+      }
+
+   </script>
+</body>
+
+</html>
---
+++ users.php
@@ -0,0 +1,148 @@
+<?php 
+    include 'includes/top.php';
+    include 'services/conn.php';
+    pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+?>
+<div class="page-header">
+   <div class="container-fluid">
+      <h2 class="h5">
+         Kullanıcılar
+      </h2>
+   </div>
+   <section class="">
+      <div class="container-fluid">
+         <div class="row">
+            <div class="col-md-12 no-padding">
+               <div class="form-group pull-left" style="max-width: 250px; margin: 0;">
+                  <div class="input-group">
+                     <input id="text" type="text" class="form-control" placeholder="ara : isim, mail, telefon">
+                  </div>
+               </div>
+               <button class="btn btn-primary pull-right no-radius" data-toggle="modal" data-target="#mdlAddUser"><i class="fa fa-plus fa-fw"></i> Ekle</button>
+               <table class="table table-hover table-striped table-bordered" id="tblUsers" style="margin-bottom:20px;">
+                  <thead>
+                     <tr>
+                        <td>#</td>
+                        <td>İsim</td>
+                        <td class="hidden-xs">Mail</td>
+                        <td class="hidden-xs">Şirket</td>
+                        <td class="hidden-xs">Pozisyon</td>
+                        <td class="hidden-xs">Telefon</td>
+                        <td>Telefon (Şirket)</td>
+                     </tr>
+                  </thead>
+                  <tbody></tbody>
+               </table>
+               <label id="tblInfo"></label>
+               <nav aria-label="Page navigation example" style="width:min-content; user-select: none;">
+                  <ul class="pagination pull-left" id="pnav" style="width:min-content"></ul>
+               </nav>
+               <span class="pull-left" style="line-height : 38px; margin-left:20px; margin-right:10px;">Limit : </span>
+               <select class="form-control pull-left" style="width: 100px;" id="slcLimit">
+                  <option value="10">10</option>
+                  <option value="15">15</option>
+                  <option value="25">25</option>
+                  <option value="50">50</option>
+               </select>
+            </div>
+         </div>
+      </div>
+   </section>
+</div>
+
+<div id="mdlAddUser" tabindex="-1" role="dialog" class="modal fade text-left show">
+   <div role="document" class="modal-dialog">
+      <div class="modal-content">
+         <div class="modal-header"><strong id="exampleModalLabel" class="modal-title">Yeni Kullanıcı Ekle</strong>
+            <button type="button" data-dismiss="modal" aria-label="Close" class="close"><span aria-hidden="true">×</span></button>
+         </div>
+         <div class="modal-body">
+            <div class="row">
+               <div class="form-group col-md-6">
+                  <label>İsim *</label>
+                  <input type="text" class="form-control" id="firstname">
+               </div>
+               <div class="form-group col-md-6">
+                  <label>Soyisim *</label>
+                  <input type="text" class="form-control" id="lastname">
+               </div>
+            </div>
+            <div class="row">
+               <div class="form-group col-md-6">
+                  <label>Mail *</label>
+                  <input type="text" class="form-control" id="mail">
+               </div>
+               <div class="form-group col-md-6">
+                  <label>Şirket *</label>
+                  <select class="form-control" id="company">
+                     <option value="1">Çelsan Çelik A.Ş.</option>
+                     <option value="2">Güneş A.Ş.</option>
+                  </select>
+               </div>
+            </div>
+            <div class="row">
+               <div class="form-group col-md-6">
+                  <label>Şifre *</label>
+                  <input type="password" class="form-control" id="password">
+               </div>
+               <div class="form-group col-md-6">
+                  <label>Şifre Tekrar *</label>
+                  <input type="password" class="form-control" id="again">
+               </div>
+            </div>
+            <div class="row">
+               <div class="form-group col-md-6">
+                  <label>Telefon *</label>
+                  <input type="text" class="form-control" maxlength="14" id="phone">
+               </div>
+               <div class="form-group col-md-6">
+                  <label>Şirket Telefonu</label>
+                  <input type="text" class="form-control" id="phone_company">
+               </div>
+            </div>
+            <div class="row">
+               <div class="form-group col-md-6">
+                  <label>Yetki *</label>
+                  <select class="form-control" id="permission">
+                  <option value="0" style="font-style:italic;">Seçiniz</option>
+                  <?php 
+                  $resYetki = pg_query("select * from yetki where id != 1");
+                  while($row = pg_fetch_object($resYetki)){
+                      echo '<option value="'.$row->id.'">'.$row->isim.'</option>';
+                  }
+                  ?>
+                  </select>
+               </div>
+            </div>
+            <label>* ile işaretli olanlar doldurulması zorunlu alanlardır.</label>
+         </div>
+         <div class="modal-footer">
+            <button type="button" data-dismiss="modal" class="btn btn-secondary">Vazgeç</button>
+            <button type="button" class="btn btn-primary" onclick="user.add();">Kaydet</button>
+         </div>
+      </div>
+   </div>
+</div>
+
+<footer class="footer">
+   <div class="footer__block block no-margin-bottom">
+      <div class="container-fluid text-center">
+         <p>Design by <a href="https://instagram.com/huseyin.yilmaz.28" target="_blank" class="external">Hüseyin Yılmaz</a></p>
+      </div>
+   </div>
+</footer>
+
+
+</div>
+</div>
+<script src="dist/vendor/jquery/jquery.min.js"></script>
+<script src="dist/vendor/popper.js/umd/popper.min.js"> </script>
+<script src="dist/vendor/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript">
+   $('.list-unstyled a[href="./users"]').parent().addClass('active');
+
+</script>
+<script src="scripts/users.js"></script>
+</body>
+
+</html>
